c916922bcb0d79580eead1a034a44a0755aed8cb
JaysinhGo <jay@paladincloud.io>
2025-02-20 21:05:59 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <h1 className="text-6xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-purple-600 to-pink-600 animate-pulse hover:scale-105 transition-transform duration-300 text-center py-8 drop-shadow-lg">
+        Hello world! Vite + React + TailwindCSS
+      </h1>
+    </>
+  );
+}
+
+export default App;
